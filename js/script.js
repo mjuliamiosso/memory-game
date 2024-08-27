3a6293d56896e9fe446d4073d18fca6dc26a45d6
Maria Júlia Miosso <mjuliamiosso@gmail.com>
2024-08-27 16:07:26 -0300

    adicionando estilos

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const cards = [
+    {image: '../img/zelda-theme/card-1.png', id: 1},
+    {image: '../img/zelda-theme/card-2.png', id: 2},
+    {image: '../img/zelda-theme/card-3.png', id: 3},
+    {image: '../img/zelda-theme/card-4.png', id: 4},
+    {image: '../img/zelda-theme/card-5.png', id: 5},
+    {image: '../img/zelda-theme/card-6.png', id: 6},
+    {image: '../img/zelda-theme/card-7.png', id: 7},
+    {image: '../img/zelda-theme/card-8.png', id: 8},
+    {image: '../img/zelda-theme/card-9.png', id: 9}
+]
