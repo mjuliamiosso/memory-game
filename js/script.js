6375babf68b9d7cba7f58a7f2ff8087095c818cc
Maria Júlia Miosso <mjuliamiosso@gmail.com>
2024-09-05 15:54:51 -0300

    add match function

--- js/script.js
+++ js/script.js
@@ -1,9 +1,24 @@
 const board = document.querySelector('.board')
 const card = document.querySelectorAll('.card')
+let hasFlippedCard = false
+let firstCard, secondCard
 
 //Virar a carta
 function flipCard(){
     this.classList.toggle('flip')
+
+    if(!hasFlippedCard){
+        //primeiro clique
+        hasFlippedCard = true
+        firstCard = this
+    } else{
+        //segundo clique
+        hasFlippedCard = false
+        secondCard = this
+        console.log(firstCard.dataset.id)
+        console.log(secondCard.dataset.id)
+        checkMatch()
+    }
 }
 
 card.forEach(card => card.addEventListener('click', flipCard))
@@ -16,3 +31,20 @@ function shuffleCards(){
 }
 
 shuffleCards()
+
+//Checar as cartas
+function checkMatch(){
+    if(firstCard.dataset.id === secondCard.dataset.id){
+        console.log('matched')
+        firstCard.removeEventListener('click', flipCard)
+        secondCard.removeEventListener('click', flipCard)
+    } else {
+        console.log('wrong')
+        setTimeout(unflipCard, 650)
+    }
+}
+
+function unflipCard(){
+    firstCard.classList.remove('flip')
+    secondCard.classList.remove('flip')
+}
