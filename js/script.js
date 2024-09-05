9476782551056843a7b390515c9a7e3d0b29790a
Maria Júlia Miosso <mjuliamiosso@gmail.com>
2024-09-05 12:13:03 -0300

    add shuffle function

--- js/script.js
+++ js/script.js
@@ -7,3 +7,12 @@ function flipCard(){
 }
 
 card.forEach(card => card.addEventListener('click', flipCard))
+
+//Bagunçar cartas
+function shuffleCards(){
+    for(let i = board.children.length; i >= 0; i--){
+        board.appendChild(board.children[Math.random()* i|0])
+    }
+}
+
+shuffleCards()
