2a79bcd66be8c1349b0ba74dbadff994364f86fe
Maria Júlia Miosso <mjuliamiosso@gmail.com>
2024-09-09 17:52:13 -0300

    add: theme change

--- js/script.js
+++ js/script.js
@@ -1,7 +1,15 @@
 const board = document.querySelector('.board')
 const card = document.querySelectorAll('.card')
+const backCard = document.querySelectorAll('.back')
+const frontCard = document.querySelectorAll('.front')
 const moves = document.querySelector('.moves')
 const themes = document.querySelector('#modal')
+const modal = document.querySelector('dialog')
+const buttonTheme = document.querySelector('.modal-close')
+const svTheme = document.querySelector("#sv-theme")
+const botwTheme = document.querySelector("#botw-theme")
+const mhwTheme = document.querySelector("#mhw-theme")
+
 let hasFlippedCard = false
 let firstCard, secondCard
 let lockBoard = false
@@ -31,6 +39,8 @@ function flipCard(){
         secondCard = this
 
         checkMatch()
+
+        //contador de jogadas
         movesCounter += 1
         moves.innerHTML = movesCounter
     }
@@ -72,13 +82,39 @@ function unflipCard(){
 }
 
 //Modal
-const modal = document.querySelector('dialog')
-const buttonTheme = document.querySelectorAll('.modal-close')
-
 themes.onclick = function (){
     modal.showModal()
 }
 
 function closeModal() {
     modal.close()
+}
+
+console.log(frontCard[3])
+
+svTheme.addEventListener('click', closeModal)
+botwTheme.addEventListener('click', closeModal)
+mhwTheme.addEventListener('click', closeModal)
+
+function changeTheme(theme){
+    const nowTheme = theme
+    
+    for (let i = 0; i < backCard.length; i++){
+        backCard[i].src=`./img/${nowTheme}/cover.png`
+    }
+
+    for (let i = 0; i < frontCard.length; i++){
+        nowSrc = frontCard[i].src
+        switch(true){
+            case frontCard[i].src.includes('botw-theme'):
+                frontCard[i].src = nowSrc.replace('botw-theme', nowTheme)
+                break
+            case frontCard[i].src.includes('sv-theme'):
+                frontCard[i].src = nowSrc.replace('sv-theme', nowTheme)
+                break
+            case frontCard[i].src.includes('mhw-theme'):
+                frontCard[i].src = nowSrc.replace('mhw-theme', nowTheme)
+                break
+        }
+    }
 }
